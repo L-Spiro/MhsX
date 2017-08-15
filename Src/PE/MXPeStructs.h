eb9db73fb6819f5bd4e9f925ef1040947f2f5a69
L-Spiro <yogurtemperor@gmail.com>
2017-08-14 22:28:34 -0700

    Fixing tabs.

--- Src/PE/MXPeStructs.h
+++ Src/PE/MXPeStructs.h
@@ -302,17 +302,17 @@ namespace mx {
 	};
 
 	struct MX_IMAGE_EXPORT_DIRECTORY {
-        uint32_t Characteristics;
-        uint32_t TimeDateStamp;
-        uint16_t MajorVersion;
-        uint16_t MinorVersion;
-        uint32_t Name;
-        uint32_t Base;
-        uint32_t NumberOfFunctions;
-        uint32_t NumberOfNames;
-        uint32_t AddressOfFunctions;
-        uint32_t AddressOfNames;
-        uint32_t AddressOfNameOrdinals;
+		uint32_t Characteristics;
+		uint32_t TimeDateStamp;
+		uint16_t MajorVersion;
+		uint16_t MinorVersion;
+		uint32_t Name;
+		uint32_t Base;
+		uint32_t NumberOfFunctions;
+		uint32_t NumberOfNames;
+		uint32_t AddressOfFunctions;
+		uint32_t AddressOfNames;
+		uint32_t AddressOfNameOrdinals;
     };
 
 	struct MX_IMAGE_RESOURCE_DIRECTORY {
