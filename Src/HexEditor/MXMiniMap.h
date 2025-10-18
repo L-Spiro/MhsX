9e322bea58eddc5d83620b96cdaece71c987de04
L-Spiro <yogurtemperor@gmail.com>
2025-10-18 15:00:27 +0900

    Adjusted mini-map organization.

--- Src/HexEditor/MXMiniMap.h
+++ Src/HexEditor/MXMiniMap.h
@@ -6,28 +6,36 @@
 
 namespace mx {
 
+	// == Types.
+	/** Mini-map immutable inputs per frame. */
+	struct MX_MINIMAP_INPUTS {
+		uint64_t								ui64TotalBytes;					/**< Total file size in bytes. */
+		uint32_t								ui32BytesPerRow;				/**< CurStyle()->uiBytesPerRow. */
+		uint64_t								ui64FirstVisibleLine;			/**< m_sdScrollView[m_eaEditAs].ui64FirstVisibleLine. */
+		int32_t									i32PageLines;					/**< m_sdScrollView[m_eaEditAs].i32PageLines (>=1). */
+		//uint32_t								ui32ScreenSize;					/**< Screen height. */
+		// Widget geometry (device/screen pixels).
+		int32_t									i32X;
+		int32_t									i32Y;
+		int32_t									i32W;
+		int32_t									i32H;
+		// Options.
+		bool									bAutoSnap;						/**< Auto Snap. */
+		uint32_t								ui32SnapLines;					/**< Snap granularity (UI edit, default 16). */
+	};
+
+	/** The mini-map data. */
+	struct MX_MINI_MAP {
+		// == Members.
+		std::vector<COLORREF>					vBuffer;						/**< The draw buffer. */
+	};
+
 	class CMiniMap {
 	public :
 		// == Enumerations.
 		/** Render flags. */
 
-		// == Types.
-		/** Mini-map immutable inputs per frame. */
-		struct MX_MINIMAP_INPUTS {
-			uint64_t								ui64TotalBytes;				/**< Total file size in bytes. */
-			uint32_t								ui32BytesPerRow;			/**< CurStyle()->uiBytesPerRow. */
-			uint64_t								ui64FirstVisibleLine;		/**< m_sdScrollView[m_eaEditAs].ui64FirstVisibleLine. */
-			int32_t									i32PageLines;				/**< m_sdScrollView[m_eaEditAs].i32PageLines (>=1). */
-			//uint32_t								ui32ScreenSize;				/**< Screen height. */
-			// Widget geometry (device/screen pixels).
-			int32_t									i32X;
-			int32_t									i32Y;
-			int32_t									i32W;
-			int32_t									i32H;
-			// Options.
-			bool									bAutoSnap;					/**< Auto Snap. */
-			uint32_t								ui32SnapLines;				/**< Snap granularity (UI edit, default 16). */
-		};
+		
 
 
 		// == Functions.
@@ -37,13 +45,9 @@ namespace mx {
 		 * \param _hDc The device context to which to draw the mini-map.
 		 * \param _miSettings The mini-map parameters.
 		 **/
-		void										DrawMiniMap( HDC _hDc, const MX_MINIMAP_INPUTS &_miSettings, uint32_t /*_ui32Flags*/ );
+		//void										DrawMiniMap( HDC _hDc, const MX_MINIMAP_INPUTS &_miSettings, uint32_t /*_ui32Flags*/ );
 
-
-
-	protected :
-		// == Members.
-		std::vector<COLORREF>						m_vBuffer;					/**< The draw buffer. */
+		
 
 		static COLORREF								m_crRainbow[16*16];			/**< Rainbow color scheme. */
 		static COLORREF								m_crAscii[16*16];			/**< ASCII color scheme. */
